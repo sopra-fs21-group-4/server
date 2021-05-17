2b30b6af7ca63a685d65a8b09dfda1ad0b464f13
Max-Zurbriggen <71377969+Max-Zurbriggen@users.noreply.github.com>
2021-05-17 10:14:23 +0200

    userServiceFriendsIntegration tests done

--- src/test/java/ch/uzh/ifi/hase/soprafs21/service/UserServiceFriendsIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/service/UserServiceFriendsIntegrationTest.java
@@ -1,35 +1,25 @@
 package ch.uzh.ifi.hase.soprafs21.service;
 
-import ch.uzh.ifi.hase.soprafs21.constant.UserStatus;
+
 import ch.uzh.ifi.hase.soprafs21.entity.User;
-import ch.uzh.ifi.hase.soprafs21.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Test class for the UserResource REST resource.
- *
- * @see UserService
- */
-//@WebAppConfiguration
+
 @SpringBootTest
-//@RunWith(SpringRunner.class)
+@RunWith(SpringRunner.class)
 public class UserServiceFriendsIntegrationTest {
 
-    @Qualifier("userRepository")
-    @Autowired
-    private UserRepository userRepository;
+//    @Qualifier("userRepository")
+//    @Autowired
+//    private UserRepository userRepository;
 
     @Autowired
     private UserService userService;
@@ -37,56 +27,57 @@ public class UserServiceFriendsIntegrationTest {
     private User user1;
     private User user2;
 
-    @BeforeEach
-    public void setup() {
-        userRepository.deleteAll();
+
+    @Test
+    @Transactional
+    public void sendFriendRequest(){
 
         // given
         user1 = new User();
         user1.setUserId(1L);
         user1.setPassword("testPW");
         user1.setUsername("testUsername");
+        user1.setEmail("test@test.test");
 
-        userService.createUser(user1);
+        user1 = userService.createUser(user1);
 
         user2 = new User();
         user2.setUserId(2L);
         user2.setPassword("testPW2");
         user2.setUsername("testUsername2");
+        user2.setEmail("test2@test.test");
 
-        userService.createUser(user2);
-
-    }
-
-
-//    @Test
-    public void sendFriendRequest(){
-
-//        System.out.println("test1-");
-//        System.out.println(user1.getOutgoingFriendRequests());
-//        System.out.println(user1.getIncomingFriendRequests());
-//        System.out.println(user2.getOutgoingFriendRequests());
-//        System.out.println(user2.getIncomingFriendRequests());
+        user2 = userService.createUser(user2);
 
         userService.sendFriendRequest(user1, user2.getUsername());
 
-        System.out.println("test2-");
-        System.out.println(user1.getOutgoingFriendRequests());
-        System.out.println(user1.getIncomingFriendRequests());
-        System.out.println(user2.getOutgoingFriendRequests());
-        System.out.println(user2.getIncomingFriendRequests());
-        System.out.println(user2.getIncomingFriendRequests().contains(user1.getUserId()));
-
-
-
         Assertions.assertTrue(user1.getOutgoingFriendRequests().contains(user2.getUserId()));
         Assertions.assertTrue(user2.getIncomingFriendRequests().contains(user1.getUserId()));
-//        System.out.println(user1.getOutgoingFriendRequests());
+
     }
 
 
-//    @Test
+    @Test
+    @Transactional
     public void acceptFriendRequest(){
+
+        // given
+        user1 = new User();
+        user1.setUserId(1L);
+        user1.setPassword("testPW");
+        user1.setUsername("testUsername");
+        user1.setEmail("test@test.test");
+
+        user1 = userService.createUser(user1);
+
+        user2 = new User();
+        user2.setUserId(2L);
+        user2.setPassword("testPW2");
+        user2.setUsername("testUsername2");
+        user2.setEmail("test2@test.test");
+
+        user2 = userService.createUser(user2);
+
 
         userService.sendFriendRequest(user1, user2.getUsername());
         userService.acceptFriendRequest(user2, user1.getUserId());
@@ -99,38 +90,31 @@ public class UserServiceFriendsIntegrationTest {
 
     }
 
-//    @Test
+    @Test
+    @Transactional
     public void rejectFriendRequest(){
+
+
+        // given
+        user1 = new User();
+        user1.setUserId(1L);
+        user1.setPassword("testPW");
+        user1.setUsername("testUsername");
+        user1.setEmail("test@test.test");
+
+        user1 = userService.createUser(user1);
+
+        user2 = new User();
+        user2.setUserId(2L);
+        user2.setPassword("testPW2");
+        user2.setUsername("testUsername2");
+        user2.setEmail("test2@test.test");
+
+        user2 = userService.createUser(user2);
 
         userService.sendFriendRequest(user1, user2.getUsername());
         userService.rejectFriendRequest(user2, user1.getUserId());
 
-        Assertions.assertTrue(!user2.getIncomingFriendRequests().contains(user1.getUserId()));
-        Assertions.assertTrue(!user1.getOutgoingFriendRequests().contains(user2.getUserId()));
-        Assertions.assertTrue(!user2.getFriends().contains(user1.getUserId()));
-        Assertions.assertTrue(!user1.getFriends().contains(user2.getUserId()));
-
-    }
-
-    //    @Test
-    public void removeFriendRequest(){
-
-        userService.sendFriendRequest(user1, user2.getUsername());
-        userService.removeFriendRequest(user1, user2.getUserId());
-
-        Assertions.assertTrue(!user2.getIncomingFriendRequests().contains(user1.getUserId()));
-        Assertions.assertTrue(!user1.getOutgoingFriendRequests().contains(user2.getUserId()));
-        Assertions.assertTrue(!user2.getFriends().contains(user1.getUserId()));
-        Assertions.assertTrue(!user1.getFriends().contains(user2.getUserId()));
-
-    }
-
-//    @Test
-    public void removeFriend(){
-
-        userService.sendFriendRequest(user1, user2.getUsername());
-        userService.acceptFriendRequest(user2, user1.getUserId());
-        userService.removeFriend(user1, user2.getUserId());
 
         Assertions.assertTrue(!user2.getIncomingFriendRequests().contains(user1.getUserId()));
         Assertions.assertTrue(!user1.getOutgoingFriendRequests().contains(user2.getUserId()));
@@ -140,7 +124,97 @@ public class UserServiceFriendsIntegrationTest {
     }
 
     @Test
+    @Transactional
+    public void removeFriendRequest(){
+
+
+
+        // given
+        user1 = new User();
+        user1.setUserId(1L);
+        user1.setPassword("testPW");
+        user1.setUsername("testUsername");
+        user1.setEmail("test@test.test");
+
+        user1 = userService.createUser(user1);
+
+        user2 = new User();
+        user2.setUserId(2L);
+        user2.setPassword("testPW2");
+        user2.setUsername("testUsername2");
+        user2.setEmail("test2@test.test");
+
+        user2 = userService.createUser(user2);
+
+
+        userService.sendFriendRequest(user1, user2.getUsername());
+        userService.removeFriendRequest(user1, user2.getUserId());
+
+
+        Assertions.assertTrue(!user2.getIncomingFriendRequests().contains(user1.getUserId()));
+        Assertions.assertTrue(!user1.getOutgoingFriendRequests().contains(user2.getUserId()));
+        Assertions.assertTrue(!user2.getFriends().contains(user1.getUserId()));
+        Assertions.assertTrue(!user1.getFriends().contains(user2.getUserId()));
+
+    }
+
+    @Test
+    @Transactional
+    public void removeFriend(){
+
+        // given
+        user1 = new User();
+        user1.setUserId(1L);
+        user1.setPassword("testPW");
+        user1.setUsername("testUsername");
+        user1.setEmail("test@test.test");
+
+        user1 = userService.createUser(user1);
+
+        user2 = new User();
+        user2.setUserId(2L);
+        user2.setPassword("testPW2");
+        user2.setUsername("testUsername2");
+        user2.setEmail("test2@test.test");
+
+        user2 = userService.createUser(user2);
+
+
+
+        userService.sendFriendRequest(user1, user2.getUsername());
+
+        userService.acceptFriendRequest(user2, user1.getUserId());
+
+        userService.removeFriend(user1, user2.getUserId());
+
+
+        Assertions.assertTrue(!user2.getIncomingFriendRequests().contains(user1.getUserId()));
+        Assertions.assertTrue(!user1.getOutgoingFriendRequests().contains(user2.getUserId()));
+        Assertions.assertTrue(!user2.getFriends().contains(user1.getUserId()));
+        Assertions.assertTrue(!user1.getFriends().contains(user2.getUserId()));
+
+    }
+
+    @Test
+    @Transactional
     public void errorTesting(){
+
+        // given
+        user1 = new User();
+        user1.setUserId(1L);
+        user1.setPassword("testPW");
+        user1.setUsername("testUsername");
+        user1.setEmail("test@test.test");
+
+        user1 = userService.createUser(user1);
+
+        user2 = new User();
+        user2.setUserId(2L);
+        user2.setPassword("testPW2");
+        user2.setUsername("testUsername2");
+        user2.setEmail("test2@test.test");
+
+        user2 = userService.createUser(user2);
 
         assertThrows(ResponseStatusException.class, ()->userService.acceptFriendRequest(user2, user1.getUserId()));
         assertThrows(ResponseStatusException.class, ()->userService.rejectFriendRequest(user2, user1.getUserId()));
