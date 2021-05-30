67e50e802b7a3194e8b35af8e1e27a837d7fab12
Aece96 <45922740+Aece96@users.noreply.github.com>
2021-05-30 10:55:38 +0200

    UserControllerTest renewed

--- src/test/java/ch/uzh/ifi/hase/soprafs21/controller/UserControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/controller/UserControllerTest.java
@@ -49,28 +49,48 @@ public class UserControllerTest {
     @MockBean
     private UserService userService;
 
+    @MockBean
+    private DTOMapper dtoMapper;
+
     //Input:
     //RequestBody --> UserPostDTO
     //ResponseBody --> UserLoginDTO
     @Test
     public void TestifUserLoginWorks() throws  Exception{
         //given
-        User user = new User();
-        user.setStatus(UserStatus.IDLE);
-        user.setEmail("firstname@lastname");
-        user.setUserId(1L);
-        user.setUsername("Thomas");
-        user.setPassword("somePassword");
-        user.setToken("someToken");
-        user.setCurrentGameId(2L);
+        User user1 = new User();
+        user1.setStatus(UserStatus.IDLE);
+        user1.setEmail("firstname@lastname");
+        user1.setUserId(1L);
+        user1.setUsername("Thomas");
+        user1.setPassword("somePassword");
+        user1.setToken("someToken");
+        user1.setCurrentGameId(2L);
 
-        //mock the UserService - returns user
-        given(userService.loginUser(Mockito.any())).willReturn(user);
+        User user2 = new User();
+        user2.setStatus(UserStatus.IDLE);
+        user2.setEmail("firstname@lastname");
+        user2.setUserId(1L);
+        user2.setUsername("Thomas");
+        user2.setPassword("somePassword");
+        user2.setToken("someotherToken");
+        user2.setCurrentGameId(2L);
 
         UserPostDTO userPostDTO = new UserPostDTO();
         userPostDTO.setUsername("Thomas");
         userPostDTO.setPassword("somePassword");
         userPostDTO.setEmail("firstname@lastname");
+
+        UserLoginDTO userLoginDTO = new UserLoginDTO();
+        userLoginDTO.setUserId(1L);
+        userLoginDTO.setUsername("Thomas");
+        userLoginDTO.setToken("someotherToken");
+
+        //mock the UserService - returns user
+        given(dtoMapper.convertUserPostDTOtoEntity(Mockito.any())).willReturn(user1);
+        given(userService.loginUser(Mockito.any())).willReturn(user2);
+        given(dtoMapper.convertEntityToUserLoginDTO(Mockito.any())).willReturn(userLoginDTO);
+
 
         // when - Input is given
         MockHttpServletRequestBuilder patchRequest = patch("/users/login")
@@ -79,9 +99,9 @@ public class UserControllerTest {
 
         // then - check the output
         mockMvc.perform(patchRequest).andExpect(status().isOk())
-                .andExpect(jsonPath("$.username", is(user.getUsername())))
-                .andExpect(jsonPath("$.userId", is(user.getUserId().intValue())))
-                .andExpect(jsonPath("$.token", is(user.getToken())));
+                .andExpect(jsonPath("$.userId", is(user2.getUserId().intValue())))
+                .andExpect(jsonPath("$.username", is(userLoginDTO.getUsername())))
+                .andExpect(jsonPath("$.token", is(userLoginDTO.getToken())));
     }
 
     @Test
@@ -94,14 +114,28 @@ public class UserControllerTest {
         user.setPassword("somePassword");
         user.setToken("someToken");
 
-        // this mocks the UserService -> we define above what the userService should return when getUsers() is called
-        given(userService.createUser(Mockito.any())).willReturn(user);
-
+        User user1 = new User();
+        user1.setUserId(1L);
+        user1.setUsername("Thomas");
+        user1.setEmail("firstname@lastname");
+        user1.setPassword("somePassword");
+        user1.setToken("someOtherToken");
 
         UserPostDTO userPostDTO = new UserPostDTO();
         userPostDTO.setUsername("Thomas");
         userPostDTO.setPassword("somePassword");
         userPostDTO.setEmail("firstname@lastname");
+
+        UserLoginDTO userLoginDTO = new UserLoginDTO();
+        userLoginDTO.setUserId(1L);
+        userLoginDTO.setUsername("Thomas");
+        userLoginDTO.setToken("someOtherToken");
+
+        // this mocks the UserService -> we define above what the userService should return when getUsers() is called
+        given(dtoMapper.convertUserPostDTOtoEntity(Mockito.any())).willReturn(user);
+        given(userService.createUser(Mockito.any())).willReturn(user1);
+        given(dtoMapper.convertEntityToUserLoginDTO(Mockito.any())).willReturn(userLoginDTO);
+
 
         // when
         MockHttpServletRequestBuilder postRequest = post("/users/create")
@@ -110,228 +144,15 @@ public class UserControllerTest {
 
         // then
         mockMvc.perform(postRequest).andExpect(status().isCreated())
-                .andExpect(jsonPath("$.username", is(user.getUsername())))
-                .andExpect(jsonPath("$.userId", is(user.getUserId().intValue())))
-                .andExpect(jsonPath("$.token", is(user.getToken())));
+                .andExpect(jsonPath("$.userId", is(user1.getUserId().intValue())))
+                .andExpect(jsonPath("$.username", is(userLoginDTO.getUsername())))
+                .andExpect(jsonPath("$.token", is(userLoginDTO.getToken())));
     }
 
     //Flow Control testing
     //Input: RequestHeader userId and RequestHeader token
     //Output: UserPrivateDTO
     //Status: OK
-//    @Test // TODO deprecated method
-    public void TestGetUsersViaUsernames() throws Exception {
-        User testUser1 = new User();
-        testUser1.setStatus(UserStatus.PLAYING);
-        testUser1.setUserId(1L);
-        testUser1.setUsername("Thomas");
-        testUser1.setCurrentGameId(2L);
-
-
-        User testUser2 = new User();
-        testUser2.setStatus(UserStatus.IDLE);
-        testUser2.setUserId(4L);
-        testUser2.setUsername("Peter");
-        testUser2.setCurrentGameId(2L);
-
-
-        User testUser3 = new User();
-        testUser3.setStatus(UserStatus.IDLE);
-        testUser3.setUserId(20L);
-        testUser3.setUsername("Müller");
-        testUser3.setCurrentGameId(2L);
-
-        //List with users
-        List<User> users = new ArrayList<>();
-        users.add(testUser1);
-        users.add(testUser2);
-        users.add(testUser3);
-
-        List<String> usernames = new ArrayList<>();
-        usernames.add(testUser1.getUsername());
-        usernames.add(testUser2.getUsername());
-        usernames.add(testUser3.getUsername());
-
-        given(userService.getUsers()).willReturn(users);
-        given(userService.getUserByUsername(usernames.get(0))).willReturn(testUser1);
-        given(userService.getUserByUsername(usernames.get(1))).willReturn(testUser2);
-        given(userService.getUserByUsername(usernames.get(2))).willReturn(testUser3);
-
-        // when --> return value should be a List of UserPublicDTOs
-        MockHttpServletRequestBuilder getRequest = get("/users")
-                .header("usernames", usernames)
-                .contentType(MediaType.APPLICATION_JSON);
-
-        // then compare UserPublicDTO Values with the user values
-        mockMvc.perform(getRequest).andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].userId", is(testUser1.getUserId().intValue())))
-                .andExpect(jsonPath("$[0].username", is(testUser1.getUsername())))
-                .andExpect(jsonPath("$[1].userId", is(testUser2.getUserId().intValue())))
-                .andExpect(jsonPath("$[1].username", is(testUser2.getUsername())))
-                .andExpect(jsonPath("$[2].userId", is(testUser3.getUserId().intValue())))
-                .andExpect(jsonPath("$[2].username", is(testUser3.getUsername())));
-    }
-
-//    @Test // TODO deprecated method
-    public void TestGetUsersViaUserIDs() throws Exception {
-        User testUser1 = new User();
-        testUser1.setStatus(UserStatus.PLAYING);
-        testUser1.setUserId(1L);
-        testUser1.setUsername("Thomas");
-        testUser1.setCurrentGameId(2L);
-
-        User testUser2 = new User();
-        testUser2.setStatus(UserStatus.IDLE);
-        testUser2.setUserId(4L);
-        testUser2.setUsername("Peter");
-        testUser2.setCurrentGameId(2L);
-
-        User testUser3 = new User();
-        testUser3.setStatus(UserStatus.IDLE);
-        testUser3.setUserId(20L);
-        testUser3.setUsername("Müller");
-        testUser3.setCurrentGameId(2L);
-
-        //List with users --> these users are fetched by three ways --> either userIds, usernames or some third category
-        List<User> users = new ArrayList<>();
-        users.add(testUser1);
-        users.add(testUser2);
-        users.add(testUser3);
-
-        //List<String> usernames = ("Thomas", "Peter", "Müller");
-        List<Long> userIds = new ArrayList<>();
-        userIds.add(testUser1.getUserId());
-        userIds.add(testUser2.getUserId());
-        userIds.add(testUser3.getUserId());
-
-        given(userService.getUsers()).willReturn(users);
-        given(userService.getUserByUserId(userIds.get(0))).willReturn(testUser1);
-        given(userService.getUserByUserId(userIds.get(1))).willReturn(testUser2);
-        given(userService.getUserByUserId(userIds.get(2))).willReturn(testUser3);
-
-        // when --> return value should be a List of UserPublicDTOs
-        MockHttpServletRequestBuilder getRequest = get("/users")
-                .header("userIds", userIds)
-                .contentType(MediaType.APPLICATION_JSON);
-
-        // then compare UserPublicDTO Values with the user values
-        mockMvc.perform(getRequest).andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].userId", is(testUser1.getUserId().intValue())))
-                .andExpect(jsonPath("$[0].username", is(testUser1.getUsername())))
-                .andExpect(jsonPath("$[1].userId", is(testUser2.getUserId().intValue())))
-                .andExpect(jsonPath("$[1].username", is(testUser2.getUsername())))
-                .andExpect(jsonPath("$[2].userId", is(testUser3.getUserId().intValue())))
-                .andExpect(jsonPath("$[2].username", is(testUser3.getUsername())));
-    }
-
-//    @Test // TODO deprecated method
-    public void TestIfGetUsersThrowsExceptionWhenBadRequest() throws Exception {
-        User testUser1 = new User();
-        testUser1.setStatus(UserStatus.PLAYING);
-        testUser1.setUserId(1L);
-        testUser1.setUsername("Thomas");
-        testUser1.setCurrentGameId(2L);
-
-
-        List<Long> userIds = new ArrayList<>();
-        userIds.add(testUser1.getUserId());
-        List<String> usernames = new ArrayList<>();
-        usernames.add(testUser1.getUsername());
-
-        // when --> if two headers are passed then the request should result in an exception
-        MockHttpServletRequestBuilder getRequest = get("/users")
-                .header("usernames", usernames)
-                .header("userIds", userIds)
-                .contentType(MediaType.APPLICATION_JSON);
-
-        // then return badRequest
-        mockMvc.perform(getRequest).andExpect(status().isBadRequest());
-    }
-
-//    @Test // TODO deprecated method
-    public void TestGtUserViaUserId() throws Exception {
-        User testUser1 = new User();
-        testUser1.setStatus(UserStatus.PLAYING);
-        testUser1.setUserId(1L);
-        testUser1.setUsername("Thomas");
-        testUser1.setCurrentGameId(2L);
-
-        given(userService.getUserByUserId(Mockito.any())).willReturn(testUser1);
-
-        // when
-        MockHttpServletRequestBuilder getRequest = get("/user")
-                .header("userId", testUser1.getUserId())
-                .contentType(MediaType.APPLICATION_JSON);
-
-        // then compare UserPublicDTO Values with the user values
-        mockMvc.perform(getRequest).andExpect(status().isOk())
-                .andExpect(jsonPath("$.userId", is(testUser1.getUserId().intValue())))
-                .andExpect(jsonPath("$.username", is(testUser1.getUsername())));
-    }
-
-//    @Test // TODO deprecated method
-    public void TestGetUserViaUsername() throws Exception {
-        User testUser1 = new User();
-        testUser1.setStatus(UserStatus.PLAYING);
-        testUser1.setUserId(1L);
-        testUser1.setUsername("Thomas");
-        testUser1.setCurrentGameId(2L);
-
-        given(userService.getUserByUsername(testUser1.getUsername())).willReturn(testUser1);
-
-        // when
-        MockHttpServletRequestBuilder getRequest = get("/user")
-                .header("username", testUser1.getUsername())
-                .contentType(MediaType.APPLICATION_JSON);
-
-        // then compare UserPublicDTO Values with the user values
-        mockMvc.perform(getRequest).andExpect(status().isOk())
-                .andExpect(jsonPath("$.userId", is(testUser1.getUserId().intValue())))
-                .andExpect(jsonPath("$.username", is(testUser1.getUsername())));
-    }
-
-//    @Test // TODO deprecated method
-    public void TestIfGetUserThrowsExceptionWhenBadRequest() throws Exception {
-        User testUser1 = new User();
-        testUser1.setStatus(UserStatus.PLAYING);
-        testUser1.setUserId(1L);
-        testUser1.setUsername("Thomas");
-        testUser1.setCurrentGameId(2L);
-
-        // when
-        MockHttpServletRequestBuilder getRequest = get("/user")
-                .header("username", testUser1.getUsername())
-                .header("userId", testUser1.getUserId())
-                .contentType(MediaType.APPLICATION_JSON);
-
-        // then return badRequest
-        mockMvc.perform(getRequest).andExpect(status().isBadRequest());
-    }
-    /*
-    //Input: userId & token as header
-    //Output: TODO might need some more attribute values; however there is a problem I don t know how to solve
-    @Test
-    public void TestgetOwnUser() throws Exception{
-        User testUser1 = new User();
-        testUser1.setStatus(UserStatus.PLAYING);
-        testUser1.setUserId(1L);
-        testUser1.setCurrentGameId(2L);
-        testUser1.setUsername("Thomas");
-        testUser1.setToken("someToken");
-
-        given(userService.verifyUser(Mockito.any(),Mockito.any())).willReturn(testUser1);
-
-        // when
-        MockHttpServletRequestBuilder getRequest = get("/me")
-                .header("userId", testUser1.getUserId())
-                .header("token", testUser1.getToken())
-                .contentType(MediaType.APPLICATION_JSON);
-
-        // then
-        mockMvc.perform(getRequest).andExpect(status().isOk())
-                                    .andExpect(jsonPath("$.userId", is(testUser1.getUserId())));
-    }
-    */
     @Test
     public void TestupdateUser() throws Exception{
         User testUser1 = new User();
@@ -341,6 +162,11 @@ public class UserControllerTest {
         testUser1.setPassword("a");
         testUser1.setUserId(2L);
 
+        User inBetween = new User();
+        inBetween.setUsername("Markus");
+        inBetween.setEmail("evenmorerandom");
+        inBetween.setPassword("somePassword");
+
         User testUser2 = new User();
         testUser2.setEmail("evenmorerandom");
         testUser2.setUsername("Markus");
@@ -348,14 +174,17 @@ public class UserControllerTest {
         testUser2.setPassword("somePassword");
         testUser2.setUserId(2L);
 
-        //mock the UserService - returns user
-        given(userService.verifyUser(Mockito.any(),Mockito.any())).willReturn(testUser1);
-        given(userService.updateUser(Mockito.any(), Mockito.any())).willReturn(testUser2);
-
         UserPutDTO userPutDTO = new UserPutDTO();
         userPutDTO.setEmail("evenmorerandom");
         userPutDTO.setUsername("Markus");
         userPutDTO.setPassword("somePassword");
+
+
+        //mock the UserService - returns user
+        given(userService.verifyUser(Mockito.any(),Mockito.any())).willReturn(testUser1);
+        given(dtoMapper.convertUserPutDTOtoEntity(Mockito.any())).willReturn(inBetween);
+        given(userService.updateUser(Mockito.any(), Mockito.any())).willReturn(testUser2);
+
 
         // when - Input is given
         MockHttpServletRequestBuilder putRequest = put("/user")
@@ -371,8 +200,7 @@ public class UserControllerTest {
                 .andExpect(jsonPath("$.token", is(testUser2.getToken())));
     }
 
-    //TODO somehow it throws an Error; I don t know why
-    /*@Test
+    @Test
     public void TestFriendRequest() throws Exception{
         User testFriend = new User();
         testFriend.setEmail("randomx");
@@ -400,9 +228,9 @@ public class UserControllerTest {
 
         // then - check the output
         mockMvc.perform(putRequest).andExpect(status().isOk());
-        verify(userService).sendFriendRequest(testUser1, testFriend.getUsername());
+        verify(userService).sendFriendRequest(testUser1, "\""+testFriend.getUsername()+"\"");
     }
-    */
+
     @Test
     public void TestFriendRemovalRequest() throws Exception{
         User testFriend = new User();
@@ -418,7 +246,6 @@ public class UserControllerTest {
         testUser1.setToken("someToken");
         testUser1.setPassword("a");
         testUser1.setUserId(2L);
-
 
         given(userService.verifyUser(Mockito.any(),Mockito.any())).willReturn(testUser1);
         doNothing().when(userService).removeFriendRequest(Mockito.any(), Mockito.any());
@@ -496,8 +323,6 @@ public class UserControllerTest {
 
         // then - check the output
         mockMvc.perform(putRequest).andExpect(status().isOk());
-        //1. check how many times userService is called
-        //2. checks if userService is called by acceptFriendRequest & if the parameters are testUser1, and testFriend.getUserId()
         verify(userService).acceptFriendRequest(testUser1, testFriend.getUserId());
     }
 
@@ -519,6 +344,7 @@ public class UserControllerTest {
 
 
         given(userService.verifyUser(Mockito.any(),Mockito.any())).willReturn(testUser1);
+        doNothing().when(userService).rejectFriendRequest(Mockito.any(),Mockito.any());
 
         // when - Input is given
         MockHttpServletRequestBuilder putRequest = put("/friends/rejectRequest")
@@ -529,6 +355,7 @@ public class UserControllerTest {
 
         // then - check the output
         mockMvc.perform(putRequest).andExpect(status().isOk());
+        verify(userService).rejectFriendRequest(testUser1, testFriend.getUserId());
     }
 
     private String asJsonString ( final Object object){
